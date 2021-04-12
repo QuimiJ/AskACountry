4a6055f59f3fedf15b14ceeedf89db62f0031ac5
QuimiJ <jaimeamann@opendeusto.es>
2021-04-12 18:28:19 +0200

    Actualizacion del metodo escribirCSV

--- src/Pregunta.c
+++ src/Pregunta.c
@@ -6,19 +6,12 @@
 
 void escribirCSV (int id, char pregunta[50], char respuestas[300],
     		char respuestaVerdadera[30], char dificultad[10], char tipo[10]) {
-	//Creacion o edicion de fichero .csv con una pregunta
-	FILE *fPPreguntas;
-	fPPreguntas = fopen("Preguntas.csv", "a");
-	fprintf(fPPreguntas, "%d, %s, %s, %s\n", id, pregunta, dificultad, tipo);
-	fclose(fPPreguntas);
-
-	//Creacion o edicion de fichero .csv con las respuestas de una pregunta
-	FILE *fPRespuestas;
-	fPRespuestas = fopen("Respuestas.csv", "a");
-	fprintf(fPRespuestas, "%d, %d, %s\n", id, 1, respuestaVerdadera);
-	//En la implementacion futura de C++ los metodos tendran que quitar la respuestaVerdadera de respuestas
-	fprintf(fPRespuestas, "%d, %d, %s\n", id, 0, respuestas);
-	fclose(fPRespuestas);
+	//Creacion o edicion de fichero .csv con una pregunta y sus respuestas
+	FILE *fPPreguntasYRespuestas;
+	fPPreguntasYRespuestas = fopen("PreguntasYRespuestas.csv", "a");
+	fprintf(fPPreguntasYRespuestas, "%d, %s, %s, %s, %d, %d, %s\n", id, pregunta, dificultad, tipo, id, 1, respuestaVerdadera);
+	fprintf(fPPreguntasYRespuestas, "%d, %s, %s, %s, %d, %d, %s\n", id, pregunta, dificultad, tipo, id, 0, respuestas);
+	fclose(fPPreguntasYRespuestas);
 }
 
 Pregunta cargarCSV (int id) {
